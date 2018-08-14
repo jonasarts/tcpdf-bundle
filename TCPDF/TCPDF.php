78786b6bdc47d7b9c66b82ec1f495e43b69b9b81
Jonas Hauser <hauser@hasch.io>
2018-08-14 22:03:09 +0200

    Added additional create methods

--- TCPDF/TCPDF.php
+++ TCPDF/TCPDF.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the TCPDF bundle package.
  *
@@ -18,10 +20,33 @@ namespace jonasarts\Bundle\TCPDFBundle\TCPDF;
  */
 class TCPDF extends \TCPDF
 {
-    public function __construct()
+    public function __construct($orientation='P', $unit='mm', $format='A4')
     {
         // construct the TCPDF class
         // __construct($orientation='P', $unit='mm', $format='A4', $unicode=true, $encoding='UTF-8', $diskcache=false, $pdfa=false)
-        parent::__construct(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        
+        //parent::__construct(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+        parent::__construct($orientation, $unit, $format, true, 'UTF-8', false);
+    }
+
+    public static function createA3($orientation='P'): self
+    {
+        return new self($orientation, 'mm', 'A3');
+    }
+
+    public static function createA4($orientation='P'): self
+    {
+        return new self($orientation, 'mm', 'A4');
+    }
+
+    public static function createA5($orientation='P'): self
+    {
+        return new self($orientation, 'mm', 'A5');
+    }
+
+    public static function createA6($orientation='P'): self
+    {
+        return new self($orientation, 'mm', 'A6');
     }
 }
