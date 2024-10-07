4d73b98a96a7b7bcb0e395a5935f215214251dc6
Jonas Hauser <hauser@hasch.io>
2024-10-07 09:41:40 +0200

    Added TCPDF Header() and Footer() closure

--- src/TCPDF/TCPDF.php
+++ src/TCPDF/TCPDF.php
@@ -15,11 +15,16 @@ namespace jonasarts\Bundle\TCPDFBundle\TCPDF;
 
 //require_once __DIR__ . '/../lib/technickcom/tcpdf/tcpdf.php';
 
+use Closure;
+
 /**
  * TCPDF Service
  */
 class TCPDF extends \TCPDF
 {
+    private ?Closure $header_closure = null;
+    private ?Closure $footer_closure = null;
+
     public function __construct($orientation='P', $unit='mm', $format='A4')
     {
         // construct the TCPDF class
@@ -28,6 +33,32 @@ class TCPDF extends \TCPDF
         //parent::__construct(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
         parent::__construct($orientation, $unit, $format, true, 'UTF-8', false);
+    }
+
+    // Page header
+    public function Header()
+    {
+        if ($this->header_closure instanceof Closure) {
+            $this->header_closure->call($this);
+        }
+    }
+
+    // Page footer
+    public function Footer()
+    {
+        if ($this->footer_closure instanceof Closure) {
+            $this->footer_closure->call($this);
+        }
+    }
+
+    public function registerHeader(Closure $closure): void
+    {
+        $this->header_closure = $closure;
+    }
+
+    public function registerFooter(Closure $closure): void
+    {
+        $this->footer_closure = $closure;
     }
 
     public static function createA3($orientation='P'): self
